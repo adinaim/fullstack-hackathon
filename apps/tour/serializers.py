0283ac80cb24b4e028508997859106311d79aa70
aigerim055 <omurzakova727@gmail.com>
2022-11-29 21:48:08 +0600

    apiview tour

--- apps/tour/serializers.py
+++ apps/tour/serializers.py
@@ -5,25 +5,18 @@ from rest_framework import serializers
 from .models import Tour, TourImage, ConcreteTour
 
 
-
-
 class TourCreateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Tour
         fields = '__all__'
 
-    title = serializers.CharField(max_length=150)
-    image = serializers.ImageField()
     tour_image_carousel = serializers.ListField(
         child=serializers.ImageField(),
         write_only=True,
         # blank=True
     )
-    guide = serializers.CharField(max_length=100)
-    place = serializers.CharField(max_length=150)
-    desc = serializers.CharField(max_length=150)
-    level = serializers.CharField(max_length=9)
-    number_of_days = serializers.IntegerField()
+
 
     def create(self, validated_data):
         # tour = Tour.objects.create(**validated_data)
--- apps/tour/views.py
+++ apps/tour/views.py
@@ -1,14 +1,20 @@
+from functools import partial
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated,
     IsAdminUser,
 )
 from rest_framework import status
+from slugify import slugify
 
 from django.contrib.auth import get_user_model
+
+from apps.tour.models import Tour
 
 from .serializers import (
     TourCreateSerializer,
@@ -21,18 +27,41 @@ from apps.business.permissions import IsOwner
 User = get_user_model()
 
 
-class TourCreateView(APIView):
+class TourView(APIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
 
     def post(self, request: Request): 
         serializer = TourCreateSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            # serializer.save(user=self.request.user)
+            serializer.save()
             return Response(
                 'Тур успешно создан!',
                 status=status.HTTP_201_CREATED
             )
 
-    # def get_permissions(self):
-    #     # return super().get_permissions()
-    #     permission_classes = [IsAuthenticated, IsOwner]
+    def get(self, request: Request):
+        # tour = Tour.objects.filter(slug=tour).first()
+        tour = Tour.objects.all()
+        serializer = TourListSerializer(tour, many=True) 
+        return Response(
+            serializer.data
+        )
+
+    def get_object(self, slug):
+        try:
+            return Tour.objects.get(slug=slug)
+        except Tour.DoesNotExist:
+            raise Http404
+
+    def put(self, request, slug):
+        tour= self.get_object(slug)
+        serializer = TourSerializer(tour, data=request.data)
+        if serializer.is_valid():
+            # t = Tour.objects.filter(slug=slug).first()
+            # t.slug = slugify(tour.title)
+            # t.save()
+            serializer.save(user=self.request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
