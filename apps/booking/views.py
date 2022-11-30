3953e04e117a234427b9d85fef1c603c1521a03f
adinaim <adinaitm@gmail.com>
2022-11-30 20:19:13 +0600

    bus profiles, guides

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,51 +1,51 @@
-# from rest_framework.viewsets import ModelViewSet
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.generics import ListAPIView
-# from django_filters import rest_framework as rest_filter
-# from rest_framework import filters
-# from rest_framework.generics import CreateAPIView
-# from rest_framework.generics import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
+from django_filters import rest_framework as rest_filter
+from rest_framework import filters
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import get_object_or_404
 
-# from .models import TourPurchase
-# from .serializers import (
-#     # TourItemsSerializer,
-#     TourPurchaseSerializer,
-#     # OrderHistorySerializer,
-# )
+from .models import TourPurchase
+from .serializers import (
+    # TourItemsSerializer,
+    TourPurchaseSerializer,
+    # OrderHistorySerializer,
+)
 
 
-# class OrderViewSet(ModelViewSet):
+class OrderViewSet(ModelViewSet):
+    serializer_class = TourPurchaseSerializer
+    permission_classes = [IsAuthenticated]   
+
+    def get_queryset(self):
+        user = self.request.user
+        return TourPurchase.objects.filter(user=user)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
+
+# class OrderHistoryView(ListAPIView):
+#     serializer_class = OrderHistorySerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         user = self.request.user
+#         return TourPurchase.objects.filter(user=user)
+       
+
+# class PurchaseCreateView(CreateAPIView):
 #     serializer_class = TourPurchaseSerializer
-#     permission_classes = [IsAuthenticated]   
 
 #     def get_queryset(self):
 #         user = self.request.user
 #         return TourPurchase.objects.filter(user=user)
 
-#     def get_serializer_context(self):
-#         context = super().get_serializer_context()
-#         context['request'] = self.request
-#         return context
-
-
-# # class OrderHistoryView(ListAPIView):
-# #     serializer_class = OrderHistorySerializer
-# #     permission_classes = [IsAuthenticated]
-
-# #     def get_queryset(self):
-# #         user = self.request.user
-# #         return TourPurchase.objects.filter(user=user)
-       
-
-# # class PurchaseCreateView(CreateAPIView):
-# #     serializer_class = TourPurchaseSerializer
-
-# #     def get_queryset(self):
-# #         user = self.request.user
-# #         return TourPurchase.objects.filter(user=user)
-
-# #     def perform_create(self, serializer):
-# #         author = get_object_or_404(TourPurchase, id=self.request.data.get('author_id'))
-# #         return serializer.save(author=author)
+#     def perform_create(self, serializer):
+#         author = get_object_or_404(TourPurchase, id=self.request.data.get('author_id'))
+#         return serializer.save(author=author)
 
     
--- apps/business/views.py
+++ apps/business/views.py
@@ -16,10 +16,8 @@ from .serializers import (
     BusinessProfileCreateSerializer,
     BusinessProfileListSerializer,
     BusinessProfileSerializer,
-    GuideCreateSerializer,
-    GuideSeriaizer,
-    GuideListSeriaizer,
-
+    GuideListSerializer,
+    GuideSerializer,
 )
 
 # class Permissions:
@@ -35,33 +33,39 @@ from .serializers import (
 #             return super().get_permissions()
 
 
-class BusinessProfileViewSet(ModelViewSet):    # update - запрашивает поле user - put
-    queryset = BusinessProfile.objects.all()
-    serializer_class = BusinessProfileSerializer
+# class BusinessProfileViewSet(ModelViewSet):    # update - запрашивает поле user - put
+#     queryset = BusinessProfile.objects.all()
+#     serializer_class = BusinessProfileSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
 
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return BusinessProfileListSerializer
-        elif self.action == 'create':
-            return BusinessProfileCreateSerializer
-        return super().get_serializer_class()
+#     def get_serializer_class(self):
+#         if self.action == 'list':
+#             return BusinessProfileListSerializer
+#         elif self.action == 'create':
+#             return BusinessProfileCreateSerializer
+#         return super().get_serializer_class()
 
-    def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
-            self.permission_classes = [AllowAny]
-        if self.action in ['create']:
-            self.permission_classes = [IsAuthenticated, IsOwner]
-        if self.action in ['destroy']:
-            self.permission_classes = [IsOwner]#, IsAdminUser]
-        if self.action in ['update', 'partial_update']:
-            self.permission_classes = [IsOwner]
-        return super().get_permissions()
+#     def get_permissions(self):
+#         if self.action in ['list', 'retrieve']:
+#             self.permission_classes = [AllowAny]
+#         if self.action in ['create']:
+#             self.permission_classes = [IsAuthenticated, IsOwner]
+#         if self.action in ['destroy']:
+#             self.permission_classes = [IsOwner]#, IsAdminUser]
+#         if self.action in ['update', 'partial_update']:
+#             self.permission_classes = [IsOwner]
+#         return super().get_permissions()
 
 
 class BusinessView(APIView):
+
+    def get(self, request: Request):
+        bus = BusinessProfile.objects.all()
+        serializer = BusinessProfileListSerializer(bus, many=True)
+
+        return Response(serializer.data)
 
     def post(self, request: Request):
         serializer = BusinessProfileCreateSerializer(data=request.data)
@@ -72,33 +76,24 @@ class BusinessView(APIView):
                 status=status.HTTP_201_CREATED
             )
 
-    def get(self, request: Request):
-        bus = BusinessProfile.objects.all()
-        serializer = BusinessProfileListSerializer(bus, many=True)
-
-        return Response(serializer.data)
-
-    def get_object(self, pk):    # permissison - for 
-        try:
-            return BusinessProfile.objects.get(slug=pk)
-        except BusinessProfile.DoesNotExist:
-            raise Http404
-
-    def put(self, request, pk):
-        bus = self.get_object(pk)
+    def put(self, request, slug):   # требует передавать поля
+        # user = request.data.get('user')
+        bus = BusinessProfile.objects.get(slug=slug)
         serializer = BusinessProfileSerializer(bus, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-    # def patch():
-    #     ...
-
-    # def destroy():
-    #     ...
+    def delete(self, request: Request, slug):
+        # user = request.data.get('user')
+        profile = BusinessProfile.objects.get(slug=slug).delete()
+        # username = request.user.username
+        # User.objects.get(username=username).delete()
+        return Response(
+            'Ваш бизнес профиль удален.',
+            status=status.HTTP_204_NO_CONTENT
+        )
 
 
 class BusinessRetrieveView(APIView):
@@ -111,21 +106,18 @@ class BusinessRetrieveView(APIView):
             raise Http404
 
 
-
-
-
 class GuideViewSet(ModelViewSet):      # update - user - 
     queryset = Guide.objects.all()
-    serializer_class = GuideSeriaizer
+    serializer_class = GuideSerializer
 
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'list':
-            return GuideListSeriaizer
+            return GuideListSerializer
         elif self.action == 'create':
-            return GuideCreateSerializer
+            return GuideSerializer
         return super().get_serializer_class()
 
     def get_permissions(self):
@@ -134,7 +126,48 @@ class GuideViewSet(ModelViewSet):      # update - user -
         if self.action in ['create']:
             self.permission_classes = [IsAuthenticated, IsOwner]
         if self.action in ['destroy']:
-            self.permission_classes = [IsOwner]#, IsAdminUser]
+            self.permission_classes = [AllowAny] #[IsOwner]#, IsAdminUser]
         if self.action in ['update', 'partial_update']:
-            self.permission_classes = [IsOwner]
+            self.permission_classes = [AllowAny] #[IsOwner]
         return super().get_permissions()
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
+
+# class GuideView(APIView):
+    
+#     def get(self, request: Request):
+#         guide = Guide.objects.all()
+#         serializer = GuideListSerializer(guide, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request: Request):
+#         serializer = GuideSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save(user=self.request.user)
+#             return Response(
+#                 'Вы успешно создали гида.', 
+#                 status=status.HTTP_201_CREATED
+#             )
+
+#     def put(self, request, slug):   # требует передавать поля
+#         # user = request.data.get('user')
+#         guide = Guide.objects.get(slug=slug)
+#         serializer = GuideSerializer(guide, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request: Request, slug):
+#         # user = request.data.get('user')
+#         guide = Guide.objects.get(slug=slug).delete()
+#         # username = request.user.username
+#         # User.objects.get(username=username).delete()
+#         return Response(
+#             'Гид удален из вашего профиля.',
+#             status=status.HTTP_204_NO_CONTENT
+#         )
