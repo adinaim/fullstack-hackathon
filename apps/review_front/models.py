2f4508338fce368c9b51414f63df1cec50048c99
aigerim055 <omurzakova727@gmail.com>
2022-12-02 15:01:44 +0600

    review models

--- apps/review_front/models.py
+++ apps/review_front/models.py
@@ -1,3 +1,70 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth import get_user_model
 
-# Create your models here.
+# from apps.business.models import Guide
+# from apps.tour_front.models import Tour
+
+
+
+# User = get_user_model()
+
+
+# RATING_CHOISES = (
+#         (1, '1'),
+#         (2, '2'),
+#         (3, '3'),
+#         (4, '4'),
+#         (5, '5')
+#     )
+
+
+# class GuideRating(models.Model):
+#     user = models.ForeignKey(
+#         to=User,
+#         on_delete=models.CASCADE,
+#         related_name='rating_guide'
+#     )
+#     guide = models.ForeignKey(
+#         to=Guide,
+#         on_delete=models.CASCADE
+#     )
+#     rating = models.CharField(max_length=1, choices=RATING_CHOISES)
+
+
+# class TourRating(models.Model):
+#     user = models.ForeignKey(
+#         to=User,
+#         on_delete=models.CASCADE,
+#         related_name='rating_tour'
+#     )
+#     tour = models.ForeignKey(
+#         to=Tour,
+#         on_delete=models.CASCADE
+#     )
+#     rating = models.CharField(max_length=1, choices=RATING_CHOISES)
+
+
+# class TourComment(models.Model):
+#     user = models.ForeignKey(
+#         to=User,
+#         on_delete=models.CASCADE,
+#         related_name='comment_tour'
+#     )
+#     tour = models.ForeignKey(
+#         to=Tour,
+#         on_delete=models.CASCADE
+#     )
+#     image = models.ImageField(upload_to='media/comment_image', blank=True)
+#     comment = models.CharField(max_length=150)
+
+
+# class TourFavorite(models.Model):
+#     user = models.ForeignKey(
+#         to=User,
+#         on_delete=models.CASCADE,
+#         related_name='favorite_tour'
+#     )
+#     tour = models.ForeignKey(
+#         to=Tour,
+#         on_delete=models.CASCADE
+#     )
--- apps/tour_front/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpletourConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.tour_front'
